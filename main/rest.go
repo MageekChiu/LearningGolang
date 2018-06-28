89aed816d1344402a74e892bdf27eb919edb96a4
mageekqiu <qiukang@mail.ustc.edu.cn>
2018-06-28 10:25:14 +0800

    file;rest json

--- main/rest.go
+++ main/rest.go
@@ -12,6 +12,8 @@ func main() {
 	router.HandleFunc("/", Index)
 	router.HandleFunc("/todos", TodoIndex)
 	router.HandleFunc("/todos/{todoId}", TodoShow)
+	router.HandleFunc("/stat/query_overview", data)
+	router.HandleFunc("/stat/query_overview/{dataId}", data)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
@@ -28,5 +30,21 @@ func TodoShow(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	todoId := vars["todoId"]
 	fmt.Fprintln(w, "Todo show:", todoId)
+}
+
+func data(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	dataId := vars["dataId"]
+	dbgInfs := []dataStruct{
+		//dataStruct{"debug", `dataId: "test.txt" Not Found`, "Cynhard"},
+		dataStruct{dataId, "data string", "mageek"},
+	}
+	fmt.Fprintln(w, dbgInfs)
+}
+
+type dataStruct struct {
+Level  string `json:"level,omitempty"` // Level解析为level,忽略空值
+Msg    string `json:"message"`                     // Msg解析为message
+author string `json:"-"`
 }
 
